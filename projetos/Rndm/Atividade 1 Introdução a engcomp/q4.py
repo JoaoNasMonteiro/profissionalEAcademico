a300ed8ccf580855af70648da163366c458e3d5d
João Pedro Nascimento Monteiro <joaoisstudying@gmail.com>
2025-01-12 10:55:20 -0300

    Atividade intro engcomp

---
+++ projetos/Rndm/Atividade 1 Introdução a engcomp/q4.py
@@ -0,0 +1,100 @@
+
+# # add sanitização de input!!!!
+# NI = int(input("Número inicial: "))
+# NF = int(input("Número final: "))
+
+# somaPares = 0
+# somaImpares = 0
+# qtdPrimos = 0
+
+# #determina se o numero é primo
+# def isPrimo(numero):
+# 	if numero < 2:
+# 		return False
+# 	for i in range(2, int(numero / 2) +1 ):
+# 		if numero % i == 0:
+# 			return False
+# 	return True
+
+
+# i = NI
+# while NI <= i <=  NF:
+
+# 	print("i = ", i)
+# 	#I é impar?
+# 	if i % 2 == 0:
+# 		print('é par')
+# 		somaPares = somaPares + i
+# 	else:
+# 		print('é impar')
+# 		somaImpares = somaImpares + i
+
+# 	if isPrimo(i):
+# 		print("é primo")
+# 		qtdPrimos += 1
+# 	else:
+# 		print("não é primo")
+
+# 	i += 1
+
+
+# print(somaPares, "pares")
+# print(somaImpares, "impares")
+# print(qtdPrimos, "primos")
+
+
+def obter_numero(mensagem):
+    while True:
+        try:
+            valor = int(input(mensagem))
+            return valor
+        except ValueError:
+            print("Por favor, insira um número inteiro válido.")
+
+# Solicita os números inicial e final
+print("Digite os valores para o intervalo.")
+while True:
+    NI = obter_numero("Número inicial: ")
+    NF = obter_numero("Número final: ")
+
+    if NI <= NF:
+        break
+    else:
+        print("O número inicial deve ser menor ou igual ao número final. Tente novamente.")
+
+somaPares = 0
+somaImpares = 0
+qtdPrimos = 0
+
+# Determina se o número é primo
+def isPrimo(numero):
+    if numero < 2:
+        return False
+    for i in range(2, int(numero / 2) + 1):
+        if numero % i == 0:
+            return False
+    return True
+
+i = NI
+while NI <= i <= NF:
+    print("i =", i)
+
+    # Verifica se é ímpar ou par
+    if i % 2 == 0:
+        print('é par')
+        somaPares += i
+    else:
+        print('é impar')
+        somaImpares += i
+
+    # Verifica se é primo
+    if isPrimo(i):
+        print("é primo")
+        qtdPrimos += 1
+    else:
+        print("não é primo")
+
+    i += 1
+
+# Exibe os resultados
+print(f"A sequência possui {qtdPrimos} números primos.\nA soma dos números pares na sequência é {somaPares}, enquanto a soma dos numeros ímapres é {somaImpares}.")
